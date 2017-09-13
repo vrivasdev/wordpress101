2825c8689178770a8a8edf00afd9ada6982513f0
vrivasdev <victor.rivas.sistemas@gmail.com>
2017-09-13 00:04:07 -0400

    Create a Theme from Scratch

---
+++ wp-content/themes/awesometheme/header.php
@@ -0,0 +1,7 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Awesome Theme</title>
+	</head>
+	<body>
---
+++ wp-content/themes/awesometheme/index.php
@@ -0,0 +1,4 @@
+
+<?php get_header();?>
+<h1> This is my index </h1>
+<?php get_footer();?>
