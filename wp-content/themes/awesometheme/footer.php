d7422939d754fdbabd47678aa1e4bb5bf2bb5aa2
vrivas <vrivas@teravisiontech.com>
2017-12-04 17:13:40 -0400

    Including CSS and JS files

--- wp-content/themes/awesometheme/footer.php
+++ wp-content/themes/awesometheme/footer.php
@@ -2,5 +2,7 @@
         <footer>
             <p> This is my footer </p>
         </footer>
+
+        <?php wp_footer();?>
     </body>
 </html>
---
+++ wp-content/themes/awesometheme/js/awesome.js
@@ -0,0 +1,3 @@
+/**
+ * Created by victor on 04/12/17.
+ */
