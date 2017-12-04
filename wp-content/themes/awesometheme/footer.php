ae7bddc0085b5a3213d77a8622fccc040a070542
vrivas <vrivas@teravisiontech.com>
2017-12-04 18:15:19 -0400

    Custom Menu

--- wp-content/themes/awesometheme/footer.php
+++ wp-content/themes/awesometheme/footer.php
@@ -1,6 +1,8 @@
 
         <footer>
             <p> This is my footer </p>
+
+            <?php wp_nav_menu(array('theme_location'=>'secondary'));?>
         </footer>
 
         <?php wp_footer();?>
