c2a0a6e049bff314475106aacbe53ba72f4c848b
Victor <victor.rivas.sistemas@gmail.com>
2018-08-30 18:17:53 -0400

    Part 2: How to properly include CSS and JS files

--- wp-content/themes/awesometheme/footer.php
+++ wp-content/themes/awesometheme/footer.php
@@ -2,5 +2,6 @@
 		<footer>
 			<p>This is my footer</p>
 		</footer>
+		<?php wp_footer();?> <!-- It calls all footer included files -->
 	</body>
 </html>
--- wp-content/themes/awesometheme/header.php
+++ wp-content/themes/awesometheme/header.php
@@ -3,5 +3,6 @@
 	<head>
 		<meta charset="UTF-8">
 		<title>Awesome Theme</title>
+		<?php wp_head();?> <!-- It calls all header included files -->
 	</head>
 	<body>
