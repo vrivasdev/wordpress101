7b0f1c2dd11a987054242dd4c788e347b1e21707
Victor Rivas <victor.rivas.sistemas@gmail.com>
2021-01-25 02:06:58 +0000

    Docker integration

--- wp-config.php
+++ wp-config.php
@@ -26,13 +26,13 @@ define('DB_NAME', 'wordpress');
 define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', '17800228');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
-define('DB_HOST', 'localhost');
+define('DB_HOST', 'mysql');
 
 /** Database Charset to use in creating database tables. */
-define('DB_CHARSET', 'utf8mb4');
+define('DB_CHARSET', 'utf8');
 
 /** The Database Collate type. Don't change this if in doubt. */
 define('DB_COLLATE', '');
